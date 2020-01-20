ecd224ead54d2af0a7ab774c04fb0901cc482fa0
Dominic Gargala <dominic.gargala@hotmail.com>
2020-01-20 16:49:17 -0500

    guest function

--- src/fishdata/fishdata.model.js
+++ src/fishdata/fishdata.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const schema = new Schema({
-    userId: { type: String, required: true },
+    userId: { type: String, required: false },
     data: { type: Schema.Types.Mixed, required: false }
 }
 );
